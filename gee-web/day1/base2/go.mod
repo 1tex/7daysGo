04860a5bd6fbf41f8d1506013f8838cdbe4a635b
tex <ty.zhao@live.com>
2022-05-11 10:32:50 +0800

    several days before...

---
+++ gee-web/day1/base2/go.mod
@@ -0,0 +1,3 @@
+module 7daysGo
+
+go 1.16
---
+++ gee-web/day1/base3/gee/main.go
@@ -0,0 +1,30 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	log.Printf("Route#{method} - #{pattern}")
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
